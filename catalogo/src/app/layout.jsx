0d74bfe202e61408df1e2ad873a56d33f63651eb
ARTHUR GON�ALVES POLO <25170150@educ123.sp.senai.br>
2026-02-05 08:42:15 -0300

    Correções

--- catalogo/src/app/layout.jsx
+++ catalogo/src/app/layout.jsx
@@ -9,6 +9,8 @@ import Footer from "@/components/blocks/footer/footer";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+// Componentes do Next
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,13 +31,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <Navbar/>
+        <Navbar />
 
         {children}
 
-      <Footer/>
+        <Footer />
         {/* script do BOOTSTRAP */}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossOrigin="anonymous"></link>
+        <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js' integrity='sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI' crossOrigin='anonymous' />
       </body>
     </html>
   );
--- catalogo/src/components/blocks/footer/footer.jsx
+++ catalogo/src/components/blocks/footer/footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (<>
-<div className="geral"  style={{backgroundColor:"#FFFFCC", height:100, justifyContent:"center"}}>
+<div className="geral h-100"  style={{backgroundColor:"#FFFFCC", justifyContent:"center", alignItems:"center"}}>
 <div className="container">
-  <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 mt-5 border-top">
+  <footer className="d-flex flex-wrap justify-content-between align-items-center mt-5" style={{minHeight:100}}>
     <p className="col-md-4 mb-0 text-muted d-flex">© 2026 Company, Leva<p2 style={{color:"#1c8cfcff"}}>Tudo</p2></p>
     <a
       href="/"
-      className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+      className="col-md-4 d-flex flex-wrap align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
     >
       <svg className="bi me-2" width={40} height={32}>
         <use xlinkHref="#bootstrap" />
